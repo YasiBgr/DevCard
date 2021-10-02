aa80c7f856518c5f6d679d68adc2da5f6368be1d
yasamanbagheri72@yahoo.com <yasamanbagheri72@yahoo.com>
2021-10-02 11:50:30 +0330

    project section from index moves to partial view Project

---
+++ DevCard-project/devCard-MVC/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace devCard_MVC.Models
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ DevCard-project/devCard-MVC/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devCard_MVC.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+    }
+}
