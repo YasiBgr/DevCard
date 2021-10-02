20528b1df2cfa1bebbd41f8dd15417ff466460b6
yasamanbagheri72@yahoo.com <yasamanbagheri72@yahoo.com>
2021-10-02 08:57:52 +0330

    Index & Contact added

--- DevCard-project/devCard-MVC/Controllers/HomeController.cs
+++ DevCard-project/devCard-MVC/Controllers/HomeController.cs
@@ -11,11 +11,11 @@ namespace devCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+       
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+           
         }
 
         public IActionResult Index()
@@ -23,10 +23,10 @@ namespace devCard_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+         public IActionResult Contact()
         {
             return View();
-        }
+        }               
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
